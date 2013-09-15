bd8b672bcece5009c67a87439736c9786f322270
Coppernine4 <coppernine4@coppernine4-desktop.(none)>
2013-09-15 11:42:57 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monstor::Application.config.secret_token = '5c57cd2d4f37d75680dbeee4de83125246f8daffe9a4cc8b5423b678ac85698ac869f224fe0e27ab06e6775b7c66c3e0af6de6c899cd897a797928e4cd42375b'
